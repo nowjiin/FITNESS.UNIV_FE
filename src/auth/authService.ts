2e9550a851f377af7eed95dad47a5f446e5ce108
nowjin <hyunjin117@naver.com>
2024-06-13 14:28:23 +0900

    ✨Feat: 로그인 여부에 따라 네비바 바뀌게 수정, 로그아웃 기능 추가

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const getToken = (): string | null => {
   return localStorage.getItem('token');
 };
@@ -12,47 +10,5 @@ const removeToken = () => {
   localStorage.removeItem('token');
 };
 
-const refreshToken = async (): Promise<string | null> => {
-  try {
-    const token = getToken();
-    if (!token) {
-      throw new Error('No token found');
-    }
 
-    const response = await axios.post('http://localhost:8080/api/refresh-token', null, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const newToken = response.data.token;
-    setToken(newToken);
-    return newToken;
-  } catch (error) {
-    console.error('Failed to refresh token:', error);
-    removeToken();
-    return null;
-  }
-};
-
-const isAuthenticated = async (): Promise<boolean> => {
-  const token = getToken();
-  if (!token) {
-    return false;
-  }
-
-  try {
-    await axios.get('http://localhost:8080/api/validate-token', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return true;
-  } catch (error) {
-    console.error('Token validation failed:', error);
-    const newToken = await refreshToken();
-    return !!newToken;
-  }
-};
-
-export { getToken, setToken, removeToken, refreshToken, isAuthenticated };
+export { getToken, setToken, removeToken };
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import NavBar from "../../components/navbar/NavBar";
+import LoginedNavBar from "../../components/navbar/LoginedNavBar";
 import NavMenuBar from "../../components/navbar/NavMenuBar";
 import CardGroups from "../../components/home/HomeCards";
 import SignupOptions from "../../components/home/SignupOptions";
 import SportsCategories from "../../components/home/SportsCategories";
 import Reviews from "../../components/home/Reviews";
 function HomePage() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <>
-      <NavBar />
+      {isAuthenticated ? <LoginedNavBar /> : <NavBar />}
       <NavMenuBar />
       <CardGroups />
       <SignupOptions />
