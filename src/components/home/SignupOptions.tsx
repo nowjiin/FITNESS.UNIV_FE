7d29d8ee9da6784834f83003362d56db1dee00b0
nowjin <hyunjin117@naver.com>
2024-06-18 17:01:33 +0900

    ✨Feat: 카드 뒤집히는 기능 추가 버튼 링크 추가

--- src/components/home/SignupOptions.tsx
+++ src/components/home/SignupOptions.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,6 +8,12 @@ import Button from "react-bootstrap/Button";
 import "./SignupOptions.scss";
 
 const SignupOptions: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <Container fluid className="m-0 p-5 bg-gray-100 h-100">
       <h2 className="text-center mb-4 responsive-text">
@@ -14,57 +21,73 @@ const SignupOptions: React.FC = () => {
       </h2>
       <Row className="justify-content-center align-items-stretch h-100">
         <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
-          <Card className="card-green-bg w-100 d-flex flex-column">
-            <Card.Body className=" d-flex flex-column">
-              <div>
-                <Card.Text className="mb-3 card-text-sm responsive-text">
-                  나에게 딱 맞는 선생님을 추천받고 싶다면?
-                </Card.Text>
-                <Card.Text className="mb-3 card-text-sm responsive-text">
-                  1분만에 회원가입하고 맞춤 제안 받아보기
-                </Card.Text>
-              </div>
-              <div className="d-flex justify-content-between align-items-center mt-auto">
-                <Button variant="">
+          <Card
+            className="card-green-bg w-100 d-flex flex-column flip-card"
+            onClick={() => handleCardClick("/mentee")}
+          >
+            <Card.Body className=" d-flex flex-column flip-card-inner">
+              <div className="flip-card-front">
+                <div>
+                  <Card.Text className="mb-3 card-text-sm responsive-text">
+                    나에게 딱 맞는 선생님을 추천받고 싶다면?
+                  </Card.Text>
+                  <Card.Text className="mb-3 card-text-sm responsive-text">
+                    1분만에 회원가입하고 맞춤 제안 받아보기
+                  </Card.Text>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-auto">
+                  <Button variant="">
+                    <img
+                      src="./main/icon-green-btn.png"
+                      alt="green button icon"
+                      style={{ width: "20px", height: "20px" }}
+                    />
+                  </Button>
                   <img
-                    src="./main/icon-green-btn.png"
+                    src="./main/airplane.png"
                     alt="green button icon"
-                    style={{ width: "20px", height: "20px" }}
+                    className="plane-img"
                   />
-                </Button>
-                <img
-                  src="./main/airplane.png"
-                  alt="green button icon"
-                  className="plane-img"
-                />
+                </div>
+              </div>
+              <div className="flip-card-back">
+                <p className="back-title">수강생 등록하기</p>
               </div>
             </Card.Body>
           </Card>
         </Col>
         <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
-          <Card className="card-blue-bg w-100 d-flex flex-column">
-            <Card.Body className="d-flex flex-column">
-              <div>
-                <Card.Text className="mb-3 card-text-sm responsive-text">
-                  나에게 딱 맞는 트레이너를 직접 찾아보고 싶다면?
-                </Card.Text>
-                <Card.Text className="mb-3 card-text-sm responsive-text">
-                  1분만에 회원가입하고 트레이너 직접 찾아보기
-                </Card.Text>
-              </div>
-              <div className="d-flex justify-content-between align-items-center mt-auto">
-                <Button variant="">
+          <Card
+            className="card-blue-bg w-100 d-flex flex-column flip-card"
+            onClick={() => handleCardClick("/mentor")}
+          >
+            <Card.Body className="d-flex flex-column flip-card-inner">
+              <div className="flip-card-front">
+                <div>
+                  <Card.Text className="mb-3 card-text-sm responsive-text">
+                    나에게 딱 맞는 트레이너를 직접 찾아보고 싶다면?
+                  </Card.Text>
+                  <Card.Text className="mb-3 card-text-sm responsive-text">
+                    1분만에 회원가입하고 트레이너 직접 찾아보기
+                  </Card.Text>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-auto">
+                  <Button variant="">
+                    <img
+                      src="./main/icon-blue-btn.png"
+                      alt="blue button icon"
+                      style={{ width: "20px", height: "20px" }}
+                    />
+                  </Button>
                   <img
-                    src="./main/icon-blue-btn.png"
-                    alt="blue button icon"
-                    style={{ width: "20px", height: "20px" }}
+                    src="./main/profile-img.png"
+                    alt="profile"
+                    className="profile-img"
                   />
-                </Button>
-                <img
-                  src="./main/profile-img.png"
-                  alt="profile"
-                  className="profile-img"
-                />
+                </div>
+              </div>
+              <div className="flip-card-back">
+                <p className="back-title">트레이너 등록하기</p>
               </div>
             </Card.Body>
           </Card>
